f1bf7525710dcd32a9b2bf70fa13852981cc5e1c
Jawn Vawka <jawn.vawka@gmail.com>
2022-05-01 19:31:35 -0500

    Add removeGrid, eraseGrid, and removeGrid and update createGrid

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-const ROWS = 32;
-const DIV_SQUARES = 32;
-
-const containerDiv = document.createElement('div');
-containerDiv.setAttribute('id', 'container');
-document.body.appendChild(containerDiv);
-
-for (i = 1; i <= ROWS; i++) {
-    const container = document.getElementById('container');
-    const row = document.createElement('div');
-    row.setAttribute('id', `row${[i]}`);
-    container.appendChild(row);
-
-    for (j = 0; j < DIV_SQUARES; j++) {
-        const parent = document.getElementById(`row${[i]}`);
-        const squareBox = document.createElement('div');
-        squareBox.setAttribute('id', `squareBox${[i]}`);
-        parent.appendChild(squareBox);
-        squareBox.addEventListener('mouseover', () => {
-            squareBox.style.backgroundColor = '#000000';
-        })
+function removeGrid(remove) {
+    for (i = 1; i <= remove; i++) {
+        let child = document.getElementById(`row${[i]}`);
+        containerDiv.removeChild(child);
     }
 }
+
+function eraseGrid() {
+        const squares = document.getElementsByClassName('squareBox');
+        for (i = 0; i < squares.length; i++) {
+            squares[i].style.backgroundColor = 'rgb(227, 227, 227)';
+        }
+}
+
+function resizeGrid() {
+    removeGrid(sliderValue);
+    sliderValue = slider.value;
+    createGrid(sliderValue);
+}
+
+function createGrid(rows) {
+    for (i = 1; i <= rows; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('id', `row${[i]}`);
+        containerDiv.appendChild(row);
+    
+        for (j = 0; j < rows; j++) {
+            const squareBox = document.createElement('div');
+            squareBox.setAttribute('class', `squareBox`);
+            row.appendChild(squareBox);
+            squareBox.addEventListener('mouseover', () => {
+                squareBox.style.backgroundColor = '#000000';
+            })
+        }
+    }
+}
+
+const containerDiv = document.getElementById('container');
+const clearBtn = document.getElementById('clear');
+let slider = document.getElementById('myRange');
+let sliderValue = slider.value;
+
+slider.addEventListener('input', resizeGrid);
+clearBtn.addEventListener('click', eraseGrid);
+
+createGrid(slider.value);
